02cb4f04f14bb35589631d4fe50a10176df2de66
brualvess <oliveirabrunna2210@gmail.com>
2022-09-21 14:23:03 -0300

    fix

--- tests/recommendationFactory.ts
+++
@@ -1,8 +0,0 @@
-import { faker } from '@faker-js/faker';
-
-export async function recommendation(){
-    return{
-        name: faker.lorem.word(10),
-        youtubeLink: "https://www.youtube.com/watch?v=1DkDrVEK2rc"
-    }
-}
